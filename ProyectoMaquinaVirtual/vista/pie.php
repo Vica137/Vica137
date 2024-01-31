115c5389d09fe51f942acf2654220b9e23a06332
gonzalonata06 <gonzalonata06@ciencias.unam.mx>
2024-01-31 13:04:31 -0600

    Se implementa nuevo css

---
+++ ProyectoMaquinaVirtual/vista/pie.php
@@ -0,0 +1,12 @@
+<hr>
+
+<center>
+<div class="site-info text-center">
+                         <p class="icenter"><a style="padding-right:17px" href="https://www.unam.mx" target="_new" rel="noopener"><img src="https://grid.unam.mx/wp-content/uploads/2023/01/l_unam.png" width="78px" height="78px" alt="UNAM" /> </a>  <a style="padding-right:17px" href="https://www.tic.unam.mx/" target="_new" rel="noopener"><img src="https://grid.unam.mx/wp-content/uploads/2023/01/dgtic_l_n.png" alt="DGTIC" width="190" height="78" /></a><a style="padding-right:17px" href="http://www.lamod.unam.mx/" target="_new" rel="noopener"><img src="https://grid.unam.mx/wp-content/uploads/2023/03/l_lamod.png" alt="LAMOD" width="208" height="78" /></a> <a style="padding-right:17px" href="https://www.atmosfera.unam.mx/" target="_new" rel="noopener"><img src="https://grid.unam.mx/wp-content/uploads/2023/01/icacc_l.png" alt="Instituto de Ciencias de la Atmósfera y Cambio Climático" width="260" height="78" /></a></p>
+ <p><a style="padding-right:17px" href="https://www.astroscu.unam.mx/IA/index.php?lang=es" target="_new" rel="noopener"><img src="https://grid.unam.mx/wp-content/uploads/2023/02/ia_nl.png" alt="Instituto de Astronomía" width="70" height="100" /></a>
+	<a style="padding-right:17px" href="https://www.nucleares.unam.mx/" target="_new" rel="noopener"><img src="https://grid.unam.mx/wp-content/uploads/2023/01/icn_l.png" alt="Instituto de Ciencias Nucleares" width="141" height="78" /></a></p>
+
+   <p style="padding-top:15px">Hecho en México. Universidad Nacional Autónoma de México (UNAM). Todos los derechos reservados 2024. Esta página puede ser reproducida con fines no lucrativos, siempre y cuando se cite la fuente completa y su dirección electrónica, y no se mutile; de otra forma requiere permiso previo por escrito de la institución. <a href="https://www.tic.unam.mx/avisosprivacidad/">| Aviso de privacidad |</a><a href="https://grid.unam.mx/index.php/creditos/"> Créditos | </a></p>
+</div>
+</center>
+
