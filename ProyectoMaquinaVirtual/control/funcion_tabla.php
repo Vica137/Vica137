9bac6193bb285e0d7c9cbdee8540334c104ffff6
gonzalonata06 <gonzalonata06@ciencias.unam.mx>
2024-02-06 15:41:09 -0600

    Diseño tabla nuevo

--- ProyectoMaquinaVirtual/control/funcion_tabla.php
+++ ProyectoMaquinaVirtual/control/funcion_tabla.php
@@ -46,45 +46,46 @@
 
 <?php
 
-function mostrarTabla($resultados) {
-    echo "<center>";
+  function mostrarTabla($resultados)
+    {
+        if (empty($resultados)) {
+            echo 'No hay datos para mostrar.';
+            return;
+        }
 
-    echo '<figure class="wp-block-table is-style-regular">';
-    echo '<table>';
-    echo '<tbody>';
-    echo '<tr bgcolor="#9fbbff">';
-    echo '<td class="has-text-align-left" data-align="left"><strong>#</strong></td>';
-    echo '<td class="has-text-align-left" data-align="left"><strong>Login</strong></td>';
-    echo '<td class="has-text-align-left" data-align="left"><strong>Cluster</strong></td>';
-    echo '<td class="has-text-align-left" data-align="left"><strong>Número de Jobs</strong></td>';
-    echo '<td class="has-text-align-left" data-align="left"><strong>Número de Horas</strong></td>';
-    echo '</font>';
-    echo '</tr>';
-
-    foreach ($resultados as $key => $fila) {
-        echo '<tr bgcolor="' . ($key % 2 == 0 ? '#ecf4fb' : '') . '">';
-        echo '<td class="has-text-align-left" data-align="left">' . ($key + 1) . '</td>';
-        echo '<td class="has-text-align-left" data-align="left">' . $fila['login'] . '</td>';
-        echo '<td class="has-text-align-left" data-align="left">' . $fila['cluster'] . '</td>';
-        echo '<td class="has-text-align-left" data-align="left">' . $fila['Njobs'] . '</td>';
-        echo '<td class="has-text-align-left" data-align="left">' . $fila['Nhoras'] . '</td>';
+        echo '<figure class="wp-block-table is-style-regular">';
+        echo '<table>';
+        echo '<tbody>';
+        echo '<tr bgcolor=#D8AEFF>';
+        echo '<td class="has-text-align-left" data-align="left"><strong>#</strong></td>';
+        foreach ($resultados[0] as $columna => $valor) {
+            echo '<td class="has-text-align-left" data-align="left"><strong>' . $columna . '</strong></td>';
+        }
         echo '</tr>';
+
+        foreach ($resultados as $key => $fila) {
+            echo '<tr bgcolor="' . ($key % 2 == 0 ? '#ecf4fb' : '') . '">';
+            echo '<td class="has-text-align-left" data-align="left">' . ($key + 1) . '</td>';
+            foreach ($fila as $valor) {
+                echo '<td class="has-text-align-left" data-align="left">' . $valor . '</td>';
+            }
+            echo '</tr>';
+        }
+
+        echo '<tr><td>TOTAL</td>';
+        echo '<td> </td>';
+        echo '<td> </td>';
+        echo '<td>' . $GLOBALS['totaljobs']  . '</td>';
+        echo '<td>' . $GLOBALS['totalhoras'] . '</td></tr>';
+        echo '</tbody>';
+        echo '</table>';
+        echo '<div class="container">';
+        echo '<figcaption class="wp-element-caption">Tabla: Resultados de la consulta. <br>Última actualización: ' . date('F Y') . '.</figcaption>';
+        echo '</div>';
+        echo '</figure>';
     }
 
-    echo '<tr>';
-    echo '<td class="has-text-align-left" data-align="left">' . ($key + 2) . '</td>';
-    echo '<td class="has-text-align-left" data-align="left">TOTAL</td>';
-    echo '<td class="has-text-align-left" data-align="left">' . $resultados[0]['cluster'] . '</td>';
-    echo '<td class="has-text-align-left" data-align="left">' . $resultados[0]['Njobs'] . '</td>';
-    echo '<td class="has-text-align-left" data-align="left">' . $resultados[0]['Nhoras'] . '</td>';
-    echo '</tr>';
 
-    echo '</tbody>';
-    echo '</table>';
-    echo '<figcaption class="wp-element-caption">Tabla: Resultados de la consulta. <br>Última actualización: ' . date('F Y') . '.</figcaption>';
-    echo '</figure>';
-    echo "</center>";
-}
 
 ?>
 
--- ProyectoMaquinaVirtual/modelo/MySqlCluster.php
+++ ProyectoMaquinaVirtual/modelo/MySqlCluster.php
@@ -1,10 +1,11 @@
 <?php
 include("conexion.php");
 
-function obtenerResultados($mes, $anio, $cluster, $ini_periodo, $fin_periodo) {
-    $totaljobs = 0;
-    $totalhoras = 0;
+// Inicializa $totaljobs y $totalhoras como variables globales
+$totaljobs = 0;
+$totalhoras = 0;
 
+function obtenerResultados($mes, $anio, $cluster, $ini_periodo, $fin_periodo ) {
     // Verificar que los parámetros son válidos antes de realizar la consulta
     if (!empty($mes) && !empty($anio) && !empty($cluster)) {
         $con = conectar();
@@ -15,42 +16,24 @@ function obtenerResultados($mes, $anio, $cluster, $ini_periodo, $fin_periodo) {
         $resultados = array();
 
         while ($row = $conta_horas->fetch_assoc()) {
-            if ($row["login"] == "TOTAL") {
-                $totaljobs += $row["Njobs"];
-                $totalhoras += $row["Nhoras"];
-            } else {
-                $resultados[] = array(
-                    'login' => substr($row["login"], 8),
-                    'cluster' => $row["cluster"],
-                    'Njobs' => $row["Njobs"],
-                    'Nhoras' => $row["Nhoras"]
-                );
-            }
+            // Sumar los valores a las variables globales
+            $GLOBALS['totaljobs'] += $row["Njobs"];
+            $GLOBALS['totalhoras'] += $row["Nhoras"];
+
+            $resultados[] = array(
+                'login' => $row['login'],
+                'cluster' => $row["cluster"],
+                'Njobs' => $row["Njobs"],
+                'Nhoras' => $row["Nhoras"]
+            );
         }
 
-        $resultados[] = array(
-            'login' => '1',
-            'cluster' => $cluster,
-            'Njobs' => $totaljobs,
-            'Nhoras' => $totalhoras
-        );
-
-     /*   // Aquí puedes imprimir o devolver los resultados según tus necesidades
-        print_r($resultados);
+        // Imprime lo que contiene el arreglos, solo en caso de saber que es lo que contiene, solo para pruebas
+        //print_r($resultados);
 
         // Cerrar la conexión después de usarla
         $con->close();
-    } else {
-        // Mensaje de error si los parámetros no son válidos
-        echo json_encode(array('error' => "Por favor, proporciona un mes, un año y un cluster válidos."));
-    }*/
-	  // Cerrar la conexión después de usarla
-        $con->close();
         return $resultados;
-
-}
+    }
 }
 ?>
-
-
-
