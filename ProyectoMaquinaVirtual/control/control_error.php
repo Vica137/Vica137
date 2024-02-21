d375a6070cada43beec95e7a716fdf402abf453b
gonzalonata06 <78128431+gonzalonata06@users.noreply.github.com>
2024-02-20 22:22:57 -0600

    Add files via upload

--- ProyectoMaquinaVirtual/control/control_error.php
+++ ProyectoMaquinaVirtual/control/control_error.php
@@ -1,37 +1,77 @@
 <?php 
-echo "Vista variable . $OpcionIndexCluster";
-print($OpcionIndexCluster);
 
+session_start();
+
+print_r($_SESSION);
+
+$OpcionIndex = $_SESSION['opcionSeleccionada'];
 //sleep(10);
 
-function redireccion(){
-        if($OpcionIndexCluster == 'cluster') {
-        header("Location: vista_cluster.php");
-	die();
-	}
-/*
-        header("Location: vista_usuario.php");
-        exit();
-        break;
-         case "proyecto":
-        header("Location: vista_proyecto.php");
-        exit();
-        break;
-        case "grid":
-        header("Location: vista_grid.php");
-        exit();
-        break;
+echo $OpcionIndex;
 
-        default:
+function redireccion(){
+	
+$OpcionIndex = $_SESSION['opcionSeleccionada'];
+	
+	switch ($OpcionIndex){
+
+	case 'cluster':
+			header("Location: ../vista/vista_cluster.php");
+			die();
+			break;
+	case 'usuario':
+        	header("Location: ../vista/vista_usuario.php");
+        	die();
+        	break;
+    case 'proyecto':
+        	header("Location: ../vista/vista_proyecto.php");
+			die();  
+        	break;
+    case 'grid':
+			header("Location: ../vista/vista_grid.php");
+			die();
+        	break;
+
+     default:
         // Página por defecto si la variable no coincide con ninguna opción
-        echo "default";
-        header("Location: ../index.php");
-        exit();
-} */
+        	//header("Location: ../index.php");
+			echo "default";	
+			die();
+			break;
+
+
+	}
+
 }
  
 
-//if (isset($_SESSION['opcionSeleccionada'])) {
+//if (isset($OpcionIndex)) {
+	$sessionParams = session_get_cookie_params();
+
+// Imprimir la configuración
+echo "Nombre de la cookie de sesión: " . $sessionParams['name'] . "<br>";
+echo "Tiempo de vida de la cookie de sesión: " . $sessionParams['lifetime'] . "<br>";
+echo "Ruta de la cookie de sesión: " . $sessionParams['path'] . "<br>";
+echo "Dominio de la cookie de sesión: " . $sessionParams['domain'] . "<br>";
+echo "Indicador de seguridad de la cookie de sesión: " . ($sessionParams['secure'] ? 'Sí' : 'No') . "<br>";
+echo "Indicador de acceso HTTPOnly de la cookie de sesión: " . ($sessionParams['httponly'] ? 'Sí' : 'No') . "<br>";
+
+if (session_status() === PHP_SESSION_ACTIVE) {
+    echo "La sesión está iniciada correctamente.";
+} else {
+    echo "La sesión no está iniciada.";
+}if (session_status() === PHP_SESSION_ACTIVE) {
+    echo "La sesión está iniciada correctamente.";
+} else {
+    echo "La sesión no está iniciada.";
+}
+
+
+echo "El ID de sesión es: " . session_id();
+
+
+   
+
     redireccion();
 //}
 ?>
