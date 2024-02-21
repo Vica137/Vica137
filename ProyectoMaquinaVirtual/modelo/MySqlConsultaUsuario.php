cee41fbe39ca8c0e38a60352471b781234add210
gonzalonata06 <78128431+gonzalonata06@users.noreply.github.com>
2024-02-20 22:30:53 -0600

    Add files via upload

--- ProyectoMaquinaVirtual/modelo/MySqlConsultaUsuario.php
+++ ProyectoMaquinaVirtual/modelo/MySqlConsultaUsuario.php
@@ -6,6 +6,12 @@ include("conexion.php");
 $totaljobs = 0;
 $totalhoras = 0;
 
+function verificarUsuario($usuario) {
+    $con = conectar();
+    $ValidarUsuario = $con->query("SELECT * FROM rgrid WHERE rgrid.login='$usuario'");
+    $con->close();
+    return $ValidarUsuario->num_rows > 0;
+}
 
 function procesarConsultaMySql($conta_horas) {
     $resultados = array();
@@ -36,32 +42,29 @@ function procesarConsultaMySql($conta_horas) {
         );
     }
 
-    //print_r($resultados);
-
     return $resultados;
 }
 
 function obtenerResultados($mes, $anio, $usuario, $ini_mes, $fin_mes, $ini_anio, $fin_anio, $todos){
     // Verificar que los parámetros son válidos antes de realizar la consulta
-    if ((!empty($mes) && !empty($anio) && !empty($usuario)) || (!empty($ini_mes) && !empty($fin_mes) && !empty($ini_anio) && !empty($fin_anio) && !empty($usuario)) || (!empty($mes) && !empty($anio) && !empty($todos)) )  {
+    if ((!empty($mes) && !empty($anio) && !empty($usuario)) || (!empty($ini_mes) && !empty($fin_mes) && !empty($ini_anio) && !empty($fin_anio) && !empty($usuario)))  {
         
         $con = conectar();
 
-        if (!empty($mes) && !empty($anio) && !empty($usuario) && empty($todos)) {
+        if (!empty($mes) && !empty($anio) && !empty($usuario)) {
             // Utilizar parámetros en la consulta
             $conta_horas = $con->query("SELECT * FROM rgrid WHERE rgrid.mes='$mes' and rgrid.anio='$anio' and rgrid.login='$usuario' ORDER BY login");
             $resultados = procesarConsultaMySql($conta_horas);
-        } elseif (!empty($ini_mes) && !empty($fin_mes) && !empty($ini_anio) && !empty($fin_anio) && !empty($usuario) && empty($todos)) {
+        } elseif (!empty($ini_mes) && !empty($fin_mes) && !empty($ini_anio) && !empty($fin_anio) && !empty($usuario)) {
             // Utilizar parámetros en la consulta
             $conta_horas = $con->query("SELECT * FROM rgrid WHERE (rgrid.anio >= '$ini_anio' AND rgrid.mes >= '$ini_mes') AND (rgrid.anio <= '$fin_anio' AND rgrid.mes <= '$fin_mes') AND rgrid.login = '$usuario' ORDER BY rgrid.anio, rgrid.mes;");
             $resultados = procesarConsultaMySql($conta_horas);
-        } elseif (!empty($mes) && !empty($anio) && $todos) {
+        } /*elseif (!empty($mes) && !empty($anio) && !empty($todos)) {
             $conta_horas = $con->query("SELECT * FROM rgrid WHERE rgrid.mes='$mes' and rgrid.anio='$anio' ORDER BY login");
-            $resultados = procesarConsultaMySql($conta_horas);
-        }
+        }*/
 
         // Imprime lo que contiene el arreglo, solo en caso de saber qué es lo que contiene, solo para pruebas
-        //print_r($resultados);
+        // print_r($resultados);
 
         // Cerrar la conexión después de usarla
         $con->close();
